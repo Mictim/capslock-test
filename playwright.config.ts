979bf22cc8f0bd0a81a3289b83bbb759dce27604
Mykhailo Tymoshenko <mykhailo.tymoshenko@sanoma.com>
2025-10-31 16:25:32 +0100

    [Config] Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? "100%" : "50%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }], ['list']],
   expect: {
     timeout: 5_000,
   },
