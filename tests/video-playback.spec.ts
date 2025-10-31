9148152d0abc845815542e3903ab9f60bf94c7bc
Mykhailo Tymoshenko <mykhailo.tymoshenko@sanoma.com>
2025-10-31 17:53:11 +0100

    [Comment] Added comments to video playback tests

--- tests/video-playback.spec.ts
+++ tests/video-playback.spec.ts
@@ -8,6 +8,10 @@ const videoPlaybackSections: Array<'Hero' | 'General View'> = [
 test.use({
     channel: 'chrome',
 })
+
+// Tests for video playback functionality can be executed only with enterprise browser channels.
+// Therefore, we set the channel to 'chrome' for this suite.
+// It will not work with 'chromium' default channel due to missing codecs in the latter.
 test.describe('Video Playback Tests', () => {
 for (const section of videoPlaybackSections) {
         test(`Validate video playback on ${section} section @video`, async ({ app }) => {
